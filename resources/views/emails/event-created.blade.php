740e3c001b4f252626da9f34ed7eda2a205bd1c5
akhilesh <akhiles@veuz.in>
2025-04-25 16:04:52 +0530

    p

---
+++ resources/views/emails/event-created.blade.php
@@ -0,0 +1,6 @@
+<h1>Event Created Successfully</h1>
+<p>Hi {{ auth()->user()->name }},</p>
+
+<p>Your event <strong>{{ $event->title }}</strong> has been created successfully!</p>
+
+<p>Thanks for using our app!</p>
